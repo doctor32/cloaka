6265f0d8e25fc7156b6cc58f8f7bd6f4805e3318
doctor32 <foraccs@meta.ua>
2023-04-10 17:54:08 +0300

    1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {onMounted} from "vue";
+
+onMounted(() => {
+  setTimeout(() => {
+    // window.location.href = 'https://cas.x-go-leads.com/click?pid=10393&offer_id=1171'
+  }, 3000)
+
+})
+
+</script>
+
+<template>
+  <main>
+    <p>Espere 3-10 segundos...</p>
+    <div class="loader"></div>
+  </main>
+</template>
+
+<style scoped >
+main {
+  width: 100%;
+  box-sizing: border-box;
+  height: 80vh;
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Arial', sans-serif;
+  flex-direction: column;
+}
+.loader {
+  background: #f8f171;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  animation: loader 2s infinite;
+}
+
+@keyframes loader {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
