f386bc6d8da1a00e48227c998144395129e9afa2
doctor32 <foraccs@meta.ua>
2023-04-22 00:21:30 +0300

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
 import {onMounted} from "vue";
 
 onMounted(() => {
-  // setTimeout(() => {
-  //   window.location.href = 'https://cas.x-go-leads.com/click?pid=10393&offer_id=1171'
-  // }, 3000)
+  setTimeout(() => {
+    window.location.href = 'https://cas.x-go-leads.com/click?pid=10393&offer_id=1171'
+  }, 3000)
 
 })
 
